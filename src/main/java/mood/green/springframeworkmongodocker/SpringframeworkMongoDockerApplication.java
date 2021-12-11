991308bea82396c6b67b84802287178323295376
Andrey <emin.f.mamedov@gmail.com>
2021-12-11 15:09:38 +0400

    Create project

---
+++ src/main/java/mood/green/springframeworkmongodocker/SpringframeworkMongoDockerApplication.java
@@ -0,0 +1,13 @@
+package mood.green.springframeworkmongodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkMongoDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringframeworkMongoDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mood/green/springframeworkmongodocker/SpringframeworkMongoDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package mood.green.springframeworkmongodocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkMongoDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
